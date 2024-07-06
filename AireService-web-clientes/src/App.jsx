fab06bf7c2788360332c33540012ede4d5070e33
ariel151515 <ndcdigital2020@gmail.com>
2024-07-06 10:21:06 -0300

    logo

---
+++ AireService-web-clientes/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './assets/logo.png'
+
+function App() {
+  return (
+    <div>
+      <img src={logo} alt="Logo" />
+      <p>Hola</p>
+    </div>
+  )
+}
+
+export default App
